5932fe864c521a3928cb1bc9b00c9eed62017596
gamerwatch <orestas.bucys@gmail.com>
2025-01-05 12:50:29 +0200

    Added Weird movement
    Plan to add normal deer Ai next

---
+++ Assets/Scripts/Deer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Deer : MonoBehaviour
+{
+    GameObject Player;
+    NavMeshAgent DeerAgent;
+    [SerializeField] LayerMask groundLayer, Playerlayer;
+    Vector3 Destpoint;
+    bool WalkPointSet;
+    [SerializeField] float range;
+    void Start()
+    {
+        DeerAgent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+    void Patrol()
+    {
+        if (!WalkPointSet) SearchForDest();
+        if (WalkPointSet) DeerAgent.SetDestination(Destpoint);
+        if(Vector3.Distance(transform.position,Destpoint) < 10) WalkPointSet = false;
+    }
+    void SearchForDest()
+    {
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
+        Destpoint = new Vector3(transform.position.x + x,transform.position.y,transform.position.z + z);
+        if (Physics.Raycast(Destpoint, Vector3.down, groundLayer))
+        {
+            WalkPointSet = true;
+        }
+    }
+
+}
